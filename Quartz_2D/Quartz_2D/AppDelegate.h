46640a10917b0e650ca86aeb54ba7f0388783876
LeverTsui <xu942829373@icloud.com>
2016-10-27 08:39:05 +0800

    Quart_2D学习Demo

---
+++ Quartz_2D/Quartz_2D/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Quartz_2D
+//
+//  Created by xulihua on 16/10/26.
+//  Copyright © 2016年 huage. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ Quartz_2D/Quartz_2D/Views/LTQuartzView.h
@@ -0,0 +1,13 @@
+//
+//  LTQuartzView.h
+//  Quartz_2D
+//
+//  Created by xulihua on 16/10/26.
+//  Copyright © 2016年 huage. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface LTQuartzView : UIView
+
+@end
