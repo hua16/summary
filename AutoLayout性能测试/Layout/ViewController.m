d10b0b42751b1e4521943743f19be074691e0f50
316495 <18682316495@163.com>
2017-03-01 16:58:41 +0800

    测试数据上传

--- AutoLayout性能测试/Layout/ViewController.m
+++ AutoLayout性能测试/Layout/ViewController.m
@@ -12,124 +12,126 @@
 
 @interface ViewController ()
 
+@property (nonatomic, strong) UITextField *textField;
+@property (nonatomic, strong) UILabel *indicateLabel;
+@property (nonatomic, strong) NSMutableArray *views;
+@property (nonatomic, strong) NSMutableDictionary *resultDictionary;
+
+@property (nonatomic, strong) UIButton *autoLayoutButton;
+@property (nonatomic, strong) UIButton *nestedButton;
+@property (nonatomic, strong) UIButton *frameButton;
+@property (nonatomic, strong) UIButton *printResult;
 @end
 
-@implementation ViewController {
-    UITextField *_textField;
-    UILabel *_indicateLabel;
-    NSMutableArray *_views;
+@implementation ViewController {}
 
-    NSMutableDictionary *_resultDictionary;
-}
-
+#pragma mark - life cycle
 - (void)viewDidLoad {
     [super viewDidLoad];
+    if ([self respondsToSelector:@selector(setAutomaticallyAdjustsScrollViewInsets:)]) {
+        self.automaticallyAdjustsScrollViewInsets = NO;
+    }
+    [self addPageSubviews];
+    [self layoutPageSubviews];
+    UITapGestureRecognizer *gesture = [[UITapGestureRecognizer alloc] initWithTarget:self action:@selector(singleTap:)];
+    [self.view addGestureRecognizer:gesture];
+}
 
-    _views = [[NSMutableArray alloc] init];
-
-    UIButton *autoLayoutButton = [[UIButton alloc] init];
-    [autoLayoutButton setTitle:@"AutoLayout" forState:UIControlStateNormal];
-    [autoLayoutButton setTitleColor:[UIColor blueColor] forState:UIControlStateNormal];
-    [autoLayoutButton addTarget:self action:@selector(generateViews)
-     forControlEvents:UIControlEventTouchUpInside];
-    [self.view addSubview:autoLayoutButton];
-    [autoLayoutButton mas_makeConstraints:^(MASConstraintMaker *make) {
-        make.height.mas_equalTo(20);
-        make.bottom.mas_equalTo(0);
-    }];
-
-    UIButton *nestedButton = [[UIButton alloc] init];
-    [nestedButton setTitle:@"Nested" forState:UIControlStateNormal];
-    [nestedButton setTitleColor:[UIColor blueColor] forState:UIControlStateNormal];
-    [nestedButton addTarget:self action:@selector(generateNestedViews)
-     forControlEvents:UIControlEventTouchUpInside];
-    [self.view addSubview:nestedButton];
-    [nestedButton mas_makeConstraints:^(MASConstraintMaker *make) {
-        make.height.mas_equalTo(20);
-        make.bottom.mas_equalTo(0);
-    }];
-
-    UIButton *frameButton = [[UIButton alloc] init];
-    [frameButton setTitle:@"Frame" forState:UIControlStateNormal];
-    [frameButton setTitleColor:[UIColor blueColor] forState:UIControlStateNormal];
-    [frameButton addTarget:self action:@selector(generateFrameViews) forControlEvents:UIControlEventTouchUpInside];
-    [self.view addSubview:frameButton];
-    [frameButton mas_makeConstraints:^(MASConstraintMaker *make) {
-        make.height.mas_equalTo(20);
-        make.bottom.mas_equalTo(0);
-    }];
-
-    [@[autoLayoutButton, nestedButton, frameButton] mas_distributeViewsAlongAxis:MASAxisTypeHorizontal
-                                                             withFixedItemLength:140 leadSpacing:0 tailSpacing:0];
-
-    _textField = [[UITextField alloc] init];
-    _textField.textAlignment = NSTextAlignmentCenter;
-    [self.view addSubview:_textField];
-    [_textField mas_makeConstraints:^(MASConstraintMaker *make) {
-        make.left.mas_equalTo(20);
-        make.right.mas_equalTo(-20);
-        make.bottom.mas_equalTo(autoLayoutButton.mas_top);
-        make.height.mas_equalTo(20);
-    }];
-
-    _indicateLabel = [[UILabel alloc] init];
-    _indicateLabel.textColor = [UIColor blackColor];
-    _indicateLabel.textAlignment = NSTextAlignmentRight;
-    [self.view addSubview:_indicateLabel];
-    [_indicateLabel mas_makeConstraints:^(MASConstraintMaker *make) {
-        make.right.top.mas_equalTo(0);
-        make.height.mas_equalTo(20);
-    }];
-
-    UIButton *printResult = [[UIButton alloc] init];
-    [printResult setTitle:@"PrintResult" forState:UIControlStateNormal];
-    [printResult setTitleColor:[UIColor blueColor] forState:UIControlStateNormal];
-    [printResult addTarget:self action:@selector(printerResult) forControlEvents:UIControlEventTouchUpInside];
-    [self.view addSubview:printResult];
-    [printResult mas_makeConstraints:^(MASConstraintMaker *make) {
-        make.height.mas_equalTo(20);
-        make.top.left.mas_equalTo(0);
-        make.width.mas_equalTo(100);
-    }];
-
-    _resultDictionary = [[NSMutableDictionary alloc] init];
-    [_resultDictionary setObject:[[NSMutableDictionary alloc] init] forKey:@"AutoLayout"];
-    [_resultDictionary setObject:[[NSMutableDictionary alloc] init] forKey:@"NestedAutoLayout"];
-    [_resultDictionary setObject:[[NSMutableDictionary alloc] init] forKey:@"Frame"];
-    [_resultDictionary setObject:[[NSMutableDictionary alloc] init] forKey:@"ASDK"];
+- (void)viewWillAppear:(BOOL)animated {
+    [super viewWillAppear:animated];
 }
 
 - (void)viewDidAppear:(BOOL)animated {
     [super viewDidAppear:animated];
-    [_textField becomeFirstResponder];
+    [self.textField becomeFirstResponder];
 }
 
+
+- (void)viewWillDisappear:(BOOL)animated {
+    [super viewWillDisappear:animated];
+}
+
+- (void)viewDidDisappear:(BOOL)animated {
+    [super viewDidDisappear:animated];
+}
+
+#pragma mark - UI & autolayout
+- (void)addPageSubviews {
+    [self.view addSubview:self.autoLayoutButton];
+    [self.view addSubview:self.nestedButton];
+    [self.view addSubview:self.frameButton];
+    [self.view addSubview:self.textField];
+    [self.view addSubview:self.indicateLabel];
+    [self.view addSubview:self.printResult];
+}
+
+- (void)layoutPageSubviews {
+    [self.autoLayoutButton mas_makeConstraints:^(MASConstraintMaker *make) {
+        make.height.mas_equalTo(20);
+        make.bottom.mas_equalTo(0);
+    }];
+    [self.nestedButton mas_makeConstraints:^(MASConstraintMaker *make) {
+        make.height.mas_equalTo(20);
+        make.bottom.mas_equalTo(0);
+    }];
+    [self.frameButton mas_makeConstraints:^(MASConstraintMaker *make) {
+        make.height.mas_equalTo(20);
+        make.bottom.mas_equalTo(0);
+    }];
+    
+    [@[self.autoLayoutButton, self.nestedButton, self.frameButton] mas_distributeViewsAlongAxis:MASAxisTypeHorizontal
+                                                                            withFixedItemLength:140 leadSpacing:0 tailSpacing:0];
+
+    [self.printResult mas_makeConstraints:^(MASConstraintMaker *make) {
+        make.height.mas_equalTo(20);
+        make.top.left.mas_equalTo(0);
+        make.width.mas_equalTo(100);
+    }];
+    
+    
+    [self.indicateLabel mas_makeConstraints:^(MASConstraintMaker *make) {
+        make.right.top.mas_equalTo(0);
+        make.width.mas_equalTo(120);
+        make.height.mas_equalTo(20);
+    }];
+    
+    
+    [self.textField mas_makeConstraints:^(MASConstraintMaker *make) {
+        make.left.mas_equalTo(self.printResult.mas_right).offset(20);
+        make.right.mas_equalTo(self.indicateLabel.mas_left).offset(-20);
+        make.top.mas_equalTo(0);
+        make.height.mas_equalTo(20);
+    }];
+}
+
+#pragma mark - event response
+
 - (void)generateViews {
-    NSInteger number = _textField.text.integerValue;
-    for (UIView *view in _views) {
+    NSInteger number = self.textField.text.integerValue;
+    for (UIView *view in self.views) {
         [view removeFromSuperview];
     }
-    _views = [[NSMutableArray alloc] init];
-
+    [self.views removeAllObjects];
+    
     NSTimeInterval startTime = [NSDate timeIntervalSinceReferenceDate];
     for (NSInteger i = 0; i < number; i++) {
         UIView *leftView = self.view;
         UIView *topView = self.view;
-        if (_views.count != 0) {
-            NSInteger left = arc4random() % _views.count;
-            NSInteger top = arc4random() % _views.count;
-            leftView = _views[left];
-            topView = _views[top];
+        if (self.views.count != 0) {
+            NSInteger left = arc4random() % self.views.count;
+            NSInteger top = arc4random() % self.views.count;
+            leftView = self.views[left];
+            topView = self.views[top];
         }
-
+        
         CGFloat hue = ( arc4random() % 256 / 256.0 );  //  0.0 to 1.0
         CGFloat saturation = ( arc4random() % 128 / 256.0 ) + 0.5;  //  0.5 to 1.0, away from white
         CGFloat brightness = ( arc4random() % 128 / 256.0 ) + 0.5;  //  0.5 to 1.0, away from black
         UIColor *color = [UIColor colorWithHue:hue saturation:saturation brightness:brightness alpha:1];
-
+        
         NSInteger leftSpace = (arc4random() % 414) - (int)leftView.frame.origin.x;
         NSInteger topSpace = (arc4random() % 568) - (int)topView.frame.origin.y;
-
+        
         UIView *newView = [[UIView alloc] init];
         newView.backgroundColor = color;
         [self.view addSubview:newView];
@@ -142,92 +144,91 @@
             make.top.mas_equalTo(topView).offset(topSpace).priorityMedium();
             make.size.mas_equalTo(10);
         }];
-
-        [_views addObject:newView];
+        
+        [self.views addObject:newView];
     }
     NSTimeInterval endTime = [NSDate timeIntervalSinceReferenceDate];
-
+    
     NSTimeInterval timeInterval = endTime - startTime;
-
-    NSMutableDictionary *autoLayoutDictionary = _resultDictionary[@"AutoLayout"];
+    
+    NSMutableDictionary *autoLayoutDictionary = self.resultDictionary[@"AutoLayout"];
     NSMutableDictionary *currentTimesDictionary = autoLayoutDictionary[@(number)] ?: [[NSMutableDictionary alloc] init];
     NSNumber *times = currentTimesDictionary[@"times"] ? : @0;
     NSNumber *avgTime = currentTimesDictionary[@"avgTime"] ? : @0;
     currentTimesDictionary[@"avgTime"] = @((times.integerValue * avgTime.doubleValue + timeInterval) / (double)(times.integerValue + 1));
     currentTimesDictionary[@"times"] = @(times.integerValue + 1);
     [autoLayoutDictionary setObject:currentTimesDictionary forKey:@(number)];
-
-    _indicateLabel.text = [NSString stringWithFormat:@"%ld: %f", (long)number, endTime-startTime];
+    
+    self.indicateLabel.text = [NSString stringWithFormat:@"%f", endTime-startTime];
 }
 
 - (void)generateFrameViews {
-    NSInteger number = _textField.text.integerValue;
-    for (UIView *view in _views) {
+    NSInteger number = self.textField.text.integerValue;
+    for (UIView *view in self.views) {
         [view removeFromSuperview];
     }
-    _views = [[NSMutableArray alloc] init];
-
+    [self.views removeAllObjects];
+    
     NSTimeInterval startTime = [NSDate timeIntervalSinceReferenceDate];
     for (NSInteger i = 0; i < number; i++) {
         CGFloat hue = ( arc4random() % 256 / 256.0 );  //  0.0 to 1.0
         CGFloat saturation = ( arc4random() % 128 / 256.0 ) + 0.5;  //  0.5 to 1.0, away from white
         CGFloat brightness = ( arc4random() % 128 / 256.0 ) + 0.5;  //  0.5 to 1.0, away from black
         UIColor *color = [UIColor colorWithHue:hue saturation:saturation brightness:brightness alpha:1];
-
+        
         NSInteger leftSpace = (arc4random() % 404) % (int)self.view.frame.size.width;
         NSInteger topSpace = (arc4random() % 676) % (int)self.view.frame.size.height + 20;
-
+        
         UIView *newView = [[UIView alloc] init];
         newView.backgroundColor = color;
         newView.frame = CGRectMake(leftSpace, topSpace, 10, 10);
         [self.view addSubview:newView];
-
-        [_views addObject:newView];
+        [self.views addObject:newView];
     }
     NSTimeInterval endTime = [NSDate timeIntervalSinceReferenceDate];
-
+    
     NSTimeInterval timeInterval = endTime - startTime;
-
-    NSMutableDictionary *frameDictionary = _resultDictionary[@"Frame"];
+    
+    NSMutableDictionary *frameDictionary = self.resultDictionary[@"Frame"];
     NSMutableDictionary *currentTimesDictionary = frameDictionary[@(number)] ?: [[NSMutableDictionary alloc] init];
     NSNumber *times = currentTimesDictionary[@"times"] ? : @0;
     NSNumber *avgTime = currentTimesDictionary[@"avgTime"] ? : @0;
     currentTimesDictionary[@"avgTime"] = @((times.integerValue * avgTime.doubleValue + timeInterval) / (double)(times.integerValue + 1));
     currentTimesDictionary[@"times"] = @(times.integerValue + 1);
     [frameDictionary setObject:currentTimesDictionary forKey:@(number)];
-
-    _indicateLabel.text = [NSString stringWithFormat:@"%ld: %f", (long)number, endTime-startTime];
+    
+    self.indicateLabel.text = [NSString stringWithFormat:@"%f", endTime-startTime];
 }
 
 - (void)generateNestedViews {
-    NSInteger number = _textField.text.integerValue;
-    for (UIView *view in _views) {
+    NSInteger number = self.textField.text.integerValue;
+    for (UIView *view in self.views) {
         [view removeFromSuperview];
     }
-    _views = [[NSMutableArray alloc] init];
-
+    [self.views removeAllObjects];
+    
     NSTimeInterval startTime = [NSDate timeIntervalSinceReferenceDate];
     for (NSInteger i = 0; i < number; i++) {
         UIView *leftView = self.view;
         UIView *topView = self.view;
-        if (_views.count != 0) {
-            NSInteger left = arc4random() % _views.count;
-            NSInteger top = arc4random() % _views.count;
-            leftView = _views[left];
-            topView = _views[top];
+        if (self.views.count != 0) {
+            NSInteger left = arc4random() % self.views.count;
+            NSInteger top = arc4random() % self.views.count;
+            leftView = self.views[left];
+            topView = self.views[top];
         }
-
+        
         CGFloat hue = ( arc4random() % 256 / 256.0 );  //  0.0 to 1.0
         CGFloat saturation = ( arc4random() % 128 / 256.0 ) + 0.5;  //  0.5 to 1.0, away from white
         CGFloat brightness = ( arc4random() % 128 / 256.0 ) + 0.5;  //  0.5 to 1.0, away from black
         UIColor *color = [UIColor colorWithHue:hue saturation:saturation brightness:brightness alpha:1];
-
+        
         UIView *newView = [[UIView alloc] init];
         newView.backgroundColor = color;
         [self.view addSubview:newView];
-        if (_views.count == 0) {
+        if (self.views.count == 0) {
             [self.view addSubview:newView];
-
+            
             [newView mas_makeConstraints:^(MASConstraintMaker *make) {
                 make.left.mas_equalTo(0.5);
                 make.top.mas_equalTo(20.5);
@@ -235,39 +236,128 @@
                 make.right.mas_equalTo(-0.5);
             }];
         } else {
-            UIView *aView = _views[i - 1];
+            UIView *aView = self.views[i - 1];
             [aView addSubview:newView];
-
+            
             [newView mas_makeConstraints:^(MASConstraintMaker *make) {
-                make.top.left.mas_equalTo(1);
-                make.bottom.right.mas_equalTo(-1);
+                make.top.left.mas_equalTo(0.5);
+                make.bottom.right.mas_equalTo(-0.5);
             }];
         }
-
-        [_views addObject:newView];
+        
+        [self.views addObject:newView];
     }
     NSTimeInterval endTime = [NSDate timeIntervalSinceReferenceDate];
-
+    
     NSTimeInterval timeInterval = endTime - startTime;
-
-    NSMutableDictionary *autoLayoutDictionary = _resultDictionary[@"NestedAutoLayout"];
+    
+    NSMutableDictionary *autoLayoutDictionary = self.resultDictionary[@"NestedAutoLayout"];
     NSMutableDictionary *currentTimesDictionary = autoLayoutDictionary[@(number)] ?: [[NSMutableDictionary alloc] init];
     NSNumber *times = currentTimesDictionary[@"times"] ? : @0;
     NSNumber *avgTime = currentTimesDictionary[@"avgTime"] ? : @0;
     currentTimesDictionary[@"avgTime"] = @((times.integerValue * avgTime.doubleValue + timeInterval) / (double)(times.integerValue + 1));
     currentTimesDictionary[@"times"] = @(times.integerValue + 1);
     [autoLayoutDictionary setObject:currentTimesDictionary forKey:@(number)];
-
-    _indicateLabel.text = [NSString stringWithFormat:@"%ld: %f", (long)number, endTime-startTime];
-
+    
+    self.indicateLabel.text = [NSString stringWithFormat:@"%f", endTime-startTime];
 }
 
 - (void)printerResult {
-    NSLog(@"%@", _resultDictionary);
+    NSLog(@"%@", self.resultDictionary);
 }
+
+- (void)singleTap:(UITapGestureRecognizer *)sender {
+    [self.textField resignFirstResponder];
+}
+
+#pragma mark - getter & setter
+- (UITextField *)textField {
+    if (!_textField) {
+        _textField = [[UITextField alloc] init];
+        _textField.textAlignment = NSTextAlignmentCenter;
+        _textField.keyboardType = UIKeyboardTypeNumberPad;
+    }
+    return _textField;
+}
+
+- (UILabel *)indicateLabel {
+    if (!_indicateLabel) {
+        _indicateLabel = [[UILabel alloc] init];
+        _indicateLabel.textColor = [UIColor blackColor];
+        _indicateLabel.textAlignment = NSTextAlignmentRight;
+    }
+    return _indicateLabel;
+}
+
+- (NSMutableArray *)views {
+    if (!_views) {
+        _views = [[NSMutableArray alloc] init];
+    }
+    return _views;
+}
+
+-(NSMutableDictionary *)resultDictionary {
+    if (!_resultDictionary) {
+        _resultDictionary = [[NSMutableDictionary alloc] init];
+        [_resultDictionary setObject:[[NSMutableDictionary alloc] init] forKey:@"AutoLayout"];
+        [_resultDictionary setObject:[[NSMutableDictionary alloc] init] forKey:@"NestedAutoLayout"];
+        [_resultDictionary setObject:[[NSMutableDictionary alloc] init] forKey:@"Frame"];
+        [_resultDictionary setObject:[[NSMutableDictionary alloc] init] forKey:@"ASDK"];
+    }
+    return _resultDictionary;
+}
+
+-(UIButton *)autoLayoutButton {
+    if (!_autoLayoutButton) {
+        _autoLayoutButton = [[UIButton alloc] init];
+        [_autoLayoutButton setTitle:@"AutoLayout" forState:UIControlStateNormal];
+        [_autoLayoutButton setTitleColor:[UIColor blueColor] forState:UIControlStateNormal];
+        [_autoLayoutButton addTarget:self
+                              action:@selector(generateViews)
+                    forControlEvents:UIControlEventTouchUpInside];
+    }
+    return _autoLayoutButton;
+}
+
+- (UIButton *)nestedButton {
+    if (!_nestedButton) {
+        _nestedButton = [[UIButton alloc] init];
+        [_nestedButton setTitle:@"Nested" forState:UIControlStateNormal];
+        [_nestedButton setTitleColor:[UIColor blueColor] forState:UIControlStateNormal];
+        [_nestedButton addTarget:self
+                          action:@selector(generateNestedViews)
+                forControlEvents:UIControlEventTouchUpInside];
+    }
+    return _nestedButton;
+}
+
+- (UIButton *)frameButton {
+    if (!_frameButton) {
+        _frameButton = [[UIButton alloc] init];
+        [_frameButton setTitle:@"Frame" forState:UIControlStateNormal];
+        [_frameButton setTitleColor:[UIColor blueColor] forState:UIControlStateNormal];
+        [_frameButton addTarget:self
+                         action:@selector(generateFrameViews)
+               forControlEvents:UIControlEventTouchUpInside];
+    }
+    return _frameButton;
+}
+
+- (UIButton *)printResult {
+    if (!_printResult) {
+        _printResult = [[UIButton alloc] init];
+        [_printResult setTitle:@"PrintResult" forState:UIControlStateNormal];
+        [_printResult setTitleColor:[UIColor blueColor] forState:UIControlStateNormal];
+        [_printResult addTarget:self
+                         action:@selector(printerResult)
+               forControlEvents:UIControlEventTouchUpInside];
+    }
+    return _printResult;
+}
+
+#pragma mark - status
 
 - (BOOL)prefersStatusBarHidden {
     return YES;
 }
-
 @end
