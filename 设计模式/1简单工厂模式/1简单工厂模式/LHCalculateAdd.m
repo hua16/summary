2ca815623c69d39050cc5620454141b19f560364
316495 <18682316495@163.com>
2017-04-28 19:24:14 +0800

    设计模式-简单工厂模式添加

---
+++ 设计模式/1简单工厂模式/1简单工厂模式/LHCalculateAdd.m
@@ -0,0 +1,15 @@
+//
+//  LHCalculateAdd.m
+//  1简单工厂模式
+//
+//  Created by huage on 15/8/10.
+//  Copyright (c) 2015年 huage. All rights reserved.
+//
+
+#import "LHCalculateAdd.h"
+@implementation LHCalculateAdd
+
+-(CGFloat)calculate{
+    return self.numberA + self.numberB;
+}
+@end
