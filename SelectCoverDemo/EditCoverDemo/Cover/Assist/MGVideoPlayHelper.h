9ba5228ef9ee7e1e5963bba289acf07d98989e83
徐利华 <18682316495@163.com>
2019-05-05 18:35:28 +0800

    fix:修复进入封面选择页当前选中的封面未显示问题

--- SelectCoverDemo/EditCoverDemo/Cover/Assist/MGVideoPlayHelper.h
+++ SelectCoverDemo/EditCoverDemo/Cover/Assist/MGVideoPlayHelper.h
@@ -12,6 +12,30 @@
 @interface MGVideoPlayHelper : NSObject
 
 /**
+ 判断视频是否横屏
+ 
+ @param videoPath 本地视频路径
+ @return 横屏，返回YES,竖屏，返回NO
+ */
++ (BOOL)isLandscape:(NSString*)videoPath;
+
+/**
+ 获取视频的分辨率
+ 
+ @param videoPath 本地视频地址
+ @return 视频分辨率
+ */
++ (CGSize)videoSizeFromVideoPath:(NSString *)videoPath;
+
+/**
+ 获取视频总时长
+ 
+ @param videoPath 本地视频地址
+ @return 视频总时长
+ */
++ (NSTimeInterval)videoDurationFromVideoPath:(NSString *)videoPath;
+
+/**
  *  根据时间，设置视频封面
  */
 + (UIImage*)getVideoCoverAtTime:(NSTimeInterval)time
